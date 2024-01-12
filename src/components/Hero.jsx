e5482d2f02aa7d61b40f464f65d32bf0322b0bb1
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2024-01-11 23:36:06 -0500

    logo updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto ">
       <div className={`${styles.paddingX} absolute inset-0 top-[30px] max-w-7xl mx-auto flex flex-row items-start gap-5 z-10`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#A0E7FF]"/>
